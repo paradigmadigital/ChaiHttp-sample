370eb95d6a81543f8fd94813ed50763c6c66d100
ncordero <ncordero@paradigmadigital.com>
2017-10-17 12:52:00 +0200

    Se agregan ultimos ejemplos de Chai HTTP

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 'use stricts';
 
 const restify = require('restify');
+var CookieParser = require('restify-cookies');
 
 const server = restify.createServer();
 
 server.get('/', function(req, res) {  
    res.send("Esto funciona!");
 });
-
+server.use(restify.CORS());
 let pais={id: 0,country:"Noruega",year:2015,days:17};
+let usuario={id: 0,user:"user",name:"Cachimiro",age:"28"};
 
 var countries = [pais];
 let lastId=0;
 
 server.use(restify.jsonBodyParser({ mapParams: true }))
+server.use(CookieParser.parse);
 
 server.post('/country',(req,res,next)=>{
 	const body=req.body;
+	console.log("---------------------------");
 	console.log(body);
+	console.log("---------------------------");
 	if(body.country==='Madrid'){
 		res.send(500,'Elemento country enviado no es un país');
+	}else if((JSON.stringify(body)).includes('&')){
+		res.send(500,'Elemento viene como un formulario. Solo se admite JSON');
 	}else{
 		body.id=++lastId;
 		countries.push(body);
@@ -57,6 +64,27 @@ server.put('/country/:id/days/:days',(req,res,next)=>{
 	countries.splice(id,1,data);
 	console.log(countries);
 	res.send(200,data);
+	next();
+});
+
+server.get('/authentication',(req,res,next)=>{
+	const body=req.body;
+	console.log(req.authorization);
+	res.setCookie('authToken','Token');
+	res.send(200,"El usuario se encuentra en la BBDD");
+	next();
+});
+
+server.get('/personalData/:user',(req,res,next)=>{
+	var user=req.params;
+	var cookies=req.cookies;
+	console.log(user);
+	console.log(cookies);
+	if(cookies.authToken==="Token"){
+		res.send(200,usuario);
+	}else{
+		res.send(500,"Se necesita el token de autenticación");
+	}
 	next();
 });
 
--- test/testChaiHTTP.js
+++ test/testChaiHTTP.js
@@ -109,3 +109,56 @@ describe('delete the country with id 1: ',()=>{
 	});
 
 });
+
+describe('Insert a country with a form: ',()=>{
+
+	it('should recieve an error because we send the country in form format', (done) => {
+		chai.request(url)
+			.post('/country')
+			.type('form')
+			.send({id:0, country: "Croacia", year: 2017, days: 10})
+			.end( function(err,res){
+				console.log(res.body)
+				expect(res).to.have.status(500);
+				done();
+			});
+	});
+});
+// The `agent` now has the sessionid cookie saved, and will send it
+// back to the server in the next request:
+var agent = chai.request.agent(url)
+describe('Authenticate a user: ',()=>{
+
+	it('should receive an OK and a cookie with the authentication token', (done) => {
+		agent
+			.get('/authentication')
+  			.auth('user', 'password')
+			.end( function(err,res){
+				console.log(res.body)
+				expect(res).to.have.cookie('authToken');
+				expect(res).to.have.status(200);
+				return agent.get('/personalData/user')
+      			.then(function (res) {
+         			expect(res).to.have.status(200);
+         			console.log(res.body)
+         			done();
+      			});
+				done();
+			});
+	});
+
+});
+
+describe('Obtain personal data without authToken: ',()=>{
+
+	it('should receive an error because we need authToken', (done) => {
+		agent
+			.get('/personalData/user')
+      		.then(function (res) {
+         		expect(res).to.have.status(500);
+         		console.log(res.body)
+      	});
+		done();
+	});
+
+});
